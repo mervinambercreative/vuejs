81349f8529b7cdc70839f63e1fe3627683dbf0d7
mervinambercreative <mervin@ambercreative.sg>
2018-08-30 13:12:15 +0800

    Vue Projects
    Vue Projects

---
+++ vue-projects/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		product: 'Socks',
+		image: 'https://images.vans.com/is/image/Vans/XRZHTG-HERO?$583x583$',
+		cdesc: '3 Socks',
+		href: 'http://www.yahoo.com',
+		width: '250px',
+		height: '250px',
+		details: ["80% cotton", "20% polyester", "Gender-neutral"],
+		inventory: 9
+	}
+})
