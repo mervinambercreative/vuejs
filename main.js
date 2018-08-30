7757de6f273508a85032c1d6be2b43bcac0fdf4c
mervinambercreative <mervin@ambercreative.sg>
2018-08-30 17:31:25 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,39 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		product: 'Socks',
-		image: 'https://images.vans.com/is/image/Vans/XRZHTG-HERO?$583x583$',
+		image: 'images/socks-red.jpg',
 		cdesc: '3 Socks',
 		href: 'http://www.yahoo.com',
 		width: '250px',
 		height: '250px',
 		details: ["80% cotton", "20% polyester", "Gender-neutral"],
-		inventory: 9
-	}
+		variants: [
+			{
+				variantId: 2234,
+				vColor: "Red",
+				vImage: 'images/socks-red.jpg'
+			},
+			{
+				variantId: 2235,
+				vColor: "Blue",
+				vImage: 'images/socks-blue.jpg'
+			}
+		],
+		inventory: 9,
+		cart: 0
+	},
+
+	methods: {
+		addToCartPlus: function() {
+			this.cart += 1
+		},
+		addToCartMinus: function() {
+			this.cart -= 1
+		},
+		updateProduct: function(vImage){
+			this.image = vImage
+		}
+	},
+
+
 })
